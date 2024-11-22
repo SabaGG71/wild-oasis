997a2eabde7e56cc25937de283d782d9dff3685f
Saba <sgachechiladze881@gmail.com>
2024-11-22 18:53:13 +0400

    Changed v1

--- app/cabins/page.jsx
+++ app/cabins/page.jsx
@@ -1,9 +1,8 @@
 import { Suspense } from "react";
-import CabinList from "../_components/CabinList";
-import Spinner from "../_components/Spinner";
-import Counter from "../_components/Counter";
-import Filter from "../_components/Filter";
-import ReservationReminder from "../_components/ReservationReminder";
+import ReservationReminder from "@/app/_components/ReservationReminder";
+import CabinList from "@/app/_components/CabinList";
+import Spinner from "@/app/_components/Spinner";
+import Filter from "@/app/_components/Filter";
 
 export const revalidate = 3600;
 // export const revalidate = 15;
